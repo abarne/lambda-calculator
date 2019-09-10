ad56c2fdb9de85598aca6c8e3622d826ae714c12
abarne <celius377@gmail.com>
2019-09-10 14:29:41 -0500

    special buttons added to screen

--- src/components/ButtonComponents/SpecialButtons/Specials.js
+++ src/components/ButtonComponents/SpecialButtons/Specials.js
@@ -1,17 +1,21 @@
 import React, { useState } from 'react';
 import { specials } from '../../../data';
+import SpecialButton from './SpecialButton';
 //import any components needed
 
 //Import your array data to from the provided data file
 
 const Specials = () => {
 	// STEP 2 - add the imported data to state
-	const [ specialsState, setSpecialsState ] = useState(specials);
+	const [ specialsState ] = useState(specials);
 	return (
-		<div>
+		<div className="special_container">
 			{/* STEP 3 - Use .map() to iterate over your array data and return a button
        component matching the name on the provided file. Pass
-       it any props needed by the child component*/}
+	   it any props needed by the child component*/
+			specialsState.map((specials, index) => <SpecialButton specials={specials} />)}
 		</div>
 	);
 };
+
+export default Specials;
